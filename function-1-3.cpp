13747922ee8d3fa874c8bf55212be57d7c7ffcf8
BigManJordan <100400782+BigManJordan@users.noreply.github.com>
2022-08-19 14:44:38 +0930

    Add files via upload

---
+++ function-1-3.cpp
@@ -0,0 +1,16 @@
+//
+//  function-1-3.cpp
+//  practical3
+//
+//  Created by Jordan Le on 19/8/2022.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+void copy_integers(int old_array[],int new_array[],int length){
+  
+  int* ptr = &old_array[0];
+  new_array = ptr;
+}
---
+++ main-1-3.cpp
@@ -0,0 +1,20 @@
+//
+//  main-1-3.cpp
+//  practical3
+//
+//  Created by Jordan Le on 19/8/2022.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+extern void copy_integers(int old_array[],int new_array[],int length);
+
+int main(void){
+  
+  int array1[3]={1,2,3};
+  int array2[3];
+  
+  copy_integers(array1, array2, 3);
+}
